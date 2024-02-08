a244ad8428bdb2ebf7267876fdf8744c5f95bd42
Jason Ching Yuen Siu <sjsa3a28@gmail.com>
2024-02-08 14:40:32 +1100

    chore: add vibrate effect - just testing

--- script.js
+++ script.js
@@ -5,18 +5,31 @@ const totalTime = 19000;
 const breatheTime = 4000;
 const holdTime = 7000;
 
-breathAnimation();
+// Vibration pattern (example: vibrate at the start of each step)
+const vibratePattern = [200]; // Vibrate for 200 milliseconds
 
 function breathAnimation() {
   text.innerText = 'Breathe In!';
   container.className = 'container grow';
+  // Trigger vibration for "Breathe In"
+  if (navigator.vibrate) {
+    navigator.vibrate(vibratePattern);
+  }
 
   setTimeout(() => {
     text.innerText = 'Hold';
+    // Trigger vibration for "Hold"
+    if (navigator.vibrate) {
+      navigator.vibrate(vibratePattern);
+    }
 
     setTimeout(() => {
       text.innerText = 'Breathe Out!';
       container.className = 'container shrink';
+      // Trigger vibration for "Breathe Out"
+      if (navigator.vibrate) {
+        navigator.vibrate(vibratePattern);
+      }
     }, holdTime);
   }, breatheTime);
 }
