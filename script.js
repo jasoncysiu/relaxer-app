04adfbb8f24c993dacafac4edf4f1ad9ca8fcea1
Jason Ching Yuen Siu <sjsa3a28@gmail.com>
2024-02-08 14:45:22 +1100

    be & bug: revert back

--- script.js
+++ script.js
@@ -5,31 +5,18 @@ const totalTime = 19000;
 const breatheTime = 4000;
 const holdTime = 7000;
 
-// Vibration pattern (example: vibrate at the start of each step)
-const vibratePattern = [200]; // Vibrate for 200 milliseconds
+breathAnimation();
 
 function breathAnimation() {
   text.innerText = 'Breathe In!';
   container.className = 'container grow';
-  // Trigger vibration for "Breathe In"
-  if (navigator.vibrate) {
-    navigator.vibrate(vibratePattern);
-  }
 
   setTimeout(() => {
     text.innerText = 'Hold';
-    // Trigger vibration for "Hold"
-    if (navigator.vibrate) {
-      navigator.vibrate(vibratePattern);
-    }
 
     setTimeout(() => {
       text.innerText = 'Breathe Out!';
       container.className = 'container shrink';
-      // Trigger vibration for "Breathe Out"
-      if (navigator.vibrate) {
-        navigator.vibrate(vibratePattern);
-      }
     }, holdTime);
   }, breatheTime);
 }
